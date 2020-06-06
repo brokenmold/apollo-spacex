09a81cc0ccfe03194aeacf0edd54402022377a14
brokenmold <jason@broken-mold.com>
2020-06-06 18:58:03 +0000

    Define getLaunchById & getLaunchByIds

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -28,6 +28,17 @@ class LaunchAPI extends RESTDataSource {
             }
         }
     }
+
+    async getLaunchById({ launchId }) {
+        const response = await trhis.get('launches', { flight_number: launchId });
+        return this.launchReducer(response[0]);
+    }
+
+    getLaunchesByIds({ launchIds }) {
+        return Promise.all(
+            launchIds.map(launchId => this.getLaunchById({ launchId })),
+        );
+    }
 }
 
 module.exports = LaunchAPI;
