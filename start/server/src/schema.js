f6a6ec9aa840d3467149ae80e87d3f85ac773d45
brokenmold <jason@broken-mold.com>
2020-06-06 04:19:09 +0000

    Created 'Launch' Object

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,7 +1,13 @@
 import { qpl } from 'apollo-server';
 
-const typeDefs = gpl `
-  // Insert Schema
+const typeDefs = gpl`
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
 `
 
 module.exports = typeDefs;
