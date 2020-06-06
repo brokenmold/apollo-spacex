3871a72de7817f67abc81c9d2a970219b934373e
brokenmold <jason@broken-mold.com>
2020-06-06 04:41:29 +0000

    Added Objects

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -8,6 +8,28 @@ const typeDefs = gpl`
     rocket: Rocket
     isBooked: Boolean!
   }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
 `
 
 module.exports = typeDefs;
