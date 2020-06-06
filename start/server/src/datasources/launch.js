d0b6a480424479a1b20bb690d32d93498e3c5b5f
brokenmold <jason@broken-mold.com>
2020-06-06 18:48:33 +0000

    Created LaunchReducer Method

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -5,6 +5,29 @@ class LaunchAPI extends RESTDataSource {
         super();
         this.baseURL = 'https://api.spacexdata.com/v2/';
     }
+
+    async getAllLaunches() {
+        const reponse = await this.get('launches');
+        return Array.isArray(reponse)
+            ? response.map(launch => this.launchReducer(launch))
+            : [];
+    }
+
+    launchReducer(launch) {
+        return {
+            id: launch.flight_number || 0,
+            cursor: `${launch.launch_date_unix}`,
+            site: launch.launch_site && launch.launch_site.site_name,
+            mission: {
+                name: launch.mission_name,
+                missionPatchSmall: launch.links.mission_patch_small,
+                missionPatchLarge: launch.links.mission_patch,
+            },
+            rocket: {
+                id: launch.rocket
+            }
+        }
+    }
 }
 
 module.exports = LaunchAPI;
