b09bba01f5a8cf58f4653b1843e50f6f32d2fb38
brokenmold <jason@broken-mold.com>
2020-06-07 02:42:27 +0000

    Setup SQLite DB

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,10 +1,20 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require ('./schema');
+const { createStore } = require('./utils');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
 
 const server = new ApolloServer({ 
     typeDefs,
     introspection: true,
     playground: true,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store }),
+    })
 });
 
 const PORT = 4010;
