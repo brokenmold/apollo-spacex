db5317055560c1b1c1f1b5161937c7e38cf7ac0f
brokenmold <jason@broken-mold.com>
2020-06-07 19:27:59 +0000

    Pass IsEmail Context to Server Function

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -23,6 +23,13 @@ module.exports = {
         };
       },
 
+      Mutation: {
+        login: async (_, { email }, { dataSources }) => {
+          const user = await dataSources.userAPI.findOrCreateUser({ email });
+          if (user) return Buffer.from(email).toString('base64');
+        }
+      },
+
       launch: (_, { id }, { dataSources }) =>
         dataSources.launchAPI.getLaunchById({ launchId: id }),
 
