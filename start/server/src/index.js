234685015da1a7c1c9bc0d094dc5d9de0c122fd6
brokenmold <jason@broken-mold.com>
2020-06-07 03:44:16 +0000

    Add Resolvers to Apollo Server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require ('./schema');
 const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
@@ -9,12 +10,13 @@ const store = createStore();
 
 const server = new ApolloServer({ 
     typeDefs,
-    introspection: true,
-    playground: true,
+    resolvers,
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store }),
-    })
+    }),
+    introspection: true,
+    playground: true,
 });
 
 const PORT = 4010;
