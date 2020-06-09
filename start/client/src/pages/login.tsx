342622bb6ef3459eda37b19c177b61b5983aa680
brokenmold <jason@broken-mold.com>
2020-06-09 00:40:41 +0000

    Update data with useMutation

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import { LoginForm, Loading } from '../components';
+import ApolloClient from 'apollo-client';
+import * as LoginTypes from './__generated__/login';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const [login, { data }] = useMutation<LoginTypes.login, LoginTypes.loginVariables>(LOGIN_USER);
+  return <LoginForm login={login} />;
 }
