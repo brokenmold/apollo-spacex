a8d65fe8c5aa2218e9b776390f389dcf52da5be6
brokenmold <jason@broken-mold.com>
2020-06-09 00:56:32 +0000

    Expose Apollo Client with useApolloClient

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -13,6 +13,19 @@ export const LOGIN_USER = gql`
 `;
 
 export default function Login() {
-  const [login, { data }] = useMutation<LoginTypes.login, LoginTypes.loginVariables>(LOGIN_USER);
+  const client: ApolloClient<any> = useApolloClient();
+  const [login, { loading, error }] = useMutation<LoginTypes.login, LoginTypes.loginVariables>(
+    LOGIN_USER,
+    {
+      onCompleted({ login }) {
+        localStorage.setItem('token', login as string);
+        client.writeData({ data: { isLoggedIn: true } });
+      }
+    }
+  );
+
+  if (loading) return <Loading />;
+  if (error) return <p>An error occurred</p>;
+
   return <LoginForm login={login} />;
 }
