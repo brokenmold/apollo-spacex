e942a82cbb3aeea92458bb1929f9963fce6e4fd5
brokenmold <jason@broken-mold.com>
2020-07-20 09:19:07 +0000

    Archive

--- final/client/src/index.tsx
+++ final/client/src/index.tsx
@@ -18,7 +18,7 @@ const cache = new InMemoryCache();
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: 'http://64.225.119.112:4020/graphql',
     headers: {
       authorization: localStorage.getItem('token'),
       'client-name': 'Space Explorer [web]',
